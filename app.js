43a3acee8f4e42ff3edad26c1c2e82613d8da089
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-11-28 21:48:20 +0000

    Add /set and /get routes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello world!');
+var cache = {};
+
+app.post('/set', function(req, res) {
+  var query = req.query;
+  Object.keys(query).forEach(function(key) {
+    cache[key] = query[key];
+  });
+  res.status(200).end();
+});
+
+app.get('/get', function(req, res) {
+  res.send(cache[req.query.key]);
 });
 
 app.listen(4000, function () {
